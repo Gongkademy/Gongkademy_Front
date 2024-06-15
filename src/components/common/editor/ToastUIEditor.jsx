b777301b588a3ad14e8329e25beeca9cfbc2db53
mung96 <mung5079@gmail.com>
2024-06-15 18:42:09 +0900

    Feat: ToastUIEditor toolbarItems 설정

--- src/components/common/editor/ToastUIEditor.jsx
+++ src/components/common/editor/ToastUIEditor.jsx
@@ -1,20 +1,24 @@
 import { Editor } from "@toast-ui/react-editor";
 import "@toast-ui/editor/toastui-editor.css";
 
-const toolbar = [
+const toolbarItems = [
   ["heading", "bold", "italic", "strike"],
-  ["hr", "quote", "ul", "ol"],
-  ["image"],
+  ["hr", "quote"],
+  ["ul", "ol", "task"],
+  ["table", "image", "link"],
 ];
 
 const ToastUIEditor = () => {
   return (
     <Editor
+      initialValue={"작성해주세요."}
+      placeholder={"작성해줘."}
       initialEditType="wysiwyg"
-      autofocus={false}
-      toolbarItems={toolbar}
+      autofocus={true}
+      toolbarItems={toolbarItems}
       hideModeSwitch
       height="500px"
+      value=""
     />
   );
 };
