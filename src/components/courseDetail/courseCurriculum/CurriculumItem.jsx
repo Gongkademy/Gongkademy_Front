1453ffdcf601b2a6252ac609a825cbd7523a49d8
mung96 <mung5079@gmail.com>
2024-06-16 13:49:28 +0900

    Refactor: 목차item을 Block태그로 묶음
    - 나중에 hover 디자인 넣을 때 편하게 하려고 변경

--- src/components/courseDetail/courseCurriculum/CurriculumItem.jsx
+++ src/components/courseDetail/courseCurriculum/CurriculumItem.jsx
@@ -1,36 +1,28 @@
 import { CheckIcon } from "@assets/svg/icons";
 import Text from "@components/common/text/Text";
-import { color } from "../../../styles/palette";
 import { Flex } from "../../common/flex/Flex";
-import { Link } from "react-router-dom";
 import { PATH } from "../../../router/Constants";
 import { COURSE_ID, LECTURE_ID } from "../../../pages/Lecture/constants";
+import { CurriculumItemBlock } from "./CurriculumItem.style";
+import { color } from "../../../styles/palette";
 const CurriculumItem = ({ isCompleted }) => {
   const lectureQueryString = `?${LECTURE_ID}=${1}&${COURSE_ID}=${1}`;
   return (
-    <Link to={PATH.LECTURE + lectureQueryString}>
-      <Flex
-        as="li"
-        align="center"
-        justify="space-between"
-        padding="1rem 0"
-        style={{ borderBottom: `1px solid ${color.gray400}` }}
-      >
-        <Flex gap="0.75rem" align="center">
-          <CheckIcon
-            width="1rem"
-            stroke={isCompleted ? color.green : color.gray400}
-          />
-          <Text typo="bodyRg700">
-            {"1"}.{"강의제목"}
-          </Text>
-        </Flex>
-
-        <Text type="captionRg400" color={color.gray400}>
-          {"10 분"}
+    <CurriculumItemBlock to={PATH.LECTURE + lectureQueryString}>
+      <Flex gap="0.75rem" align="center">
+        <CheckIcon
+          width="1rem"
+          stroke={isCompleted ? color.green : color.gray400}
+        />
+        <Text typo="bodyRg700">
+          {"1"}.{"강의제목"}
         </Text>
       </Flex>
-    </Link>
+
+      <Text type="captionRg400" color={color.gray400}>
+        {"10 분"}
+      </Text>
+    </CurriculumItemBlock>
   );
 };
 
