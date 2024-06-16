d815a6e4117dd5b2a165dd051d5185d6f4f6dcb7
mung96 <mung5079@gmail.com>
2024-06-16 13:28:47 +0900

    Fix: LecturePage 버그 수정
    - ArrowRightIcon 연결 수정
    - 강의 수강플레이어 rem 단위 수정 (임시로 해놓음)

--- src/components/courseDetail/courseCurriculum/CurriculumItem.jsx
+++ src/components/courseDetail/courseCurriculum/CurriculumItem.jsx
@@ -2,29 +2,32 @@ import { CheckIcon } from "@assets/svg/icons";
 import Text from "@components/common/text/Text";
 import { color } from "../../../styles/palette";
 import { Flex } from "../../common/flex/Flex";
+import { Link } from "react-router-dom";
 const CurriculumItem = ({ isCompleted }) => {
   return (
-    <Flex
-      as="li"
-      align="center"
-      justify="space-between"
-      padding="1rem 0"
-      style={{ borderBottom: `1px solid ${color.gray400}` }}
-    >
-      <Flex gap="0.75rem" align="center">
-        <CheckIcon
-          width="1rem"
-          stroke={isCompleted ? color.green : color.gray400}
-        />
-        <Text typo="bodyRg700">
-          {"1"}.{"강의제목"}
+    <Link to="">
+      <Flex
+        as="li"
+        align="center"
+        justify="space-between"
+        padding="1rem 0"
+        style={{ borderBottom: `1px solid ${color.gray400}` }}
+      >
+        <Flex gap="0.75rem" align="center">
+          <CheckIcon
+            width="1rem"
+            stroke={isCompleted ? color.green : color.gray400}
+          />
+          <Text typo="bodyRg700">
+            {"1"}.{"강의제목"}
+          </Text>
+        </Flex>
+
+        <Text type="captionRg400" color={color.gray400}>
+          {"10 분"}
         </Text>
       </Flex>
-
-      <Text type="captionRg400" color={color.gray400}>
-        {"10 분"}
-      </Text>
-    </Flex>
+    </Link>
   );
 };
 
