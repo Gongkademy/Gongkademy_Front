5248ac3f7ca90afd4227bb686889efeb502a8c85
KIMHT98 <kht19684@gmail.com>
2024-06-25 22:51:33 +0900

    CommunityCard로 Card통일

--- src/components/community/communityBoard/CommunityBoardPage.jsx
+++ src/components/community/communityBoard/CommunityBoardPage.jsx
@@ -1,45 +1,37 @@
-import {
-  useLocation,
-  Link,
-} from "react-router-dom";
+import { Link } from "react-router-dom";
 import { PATH } from "@router/Constants";
 import NoticeCard from "@components/community/Card/NoticeCard";
 import { NOTICE_LIST } from "@dummy/Notice";
 import { QNA_LIST } from "@dummy/Qna";
-import QnaCard from "@components/community/Card/QnaCard";
-import ConcernCard from "@components/community/Card/ConcernCard";
+import CommunityCard from "@components/community/Card/CommunityCard";
 import { CONCERN_LIST } from "@dummy/Concern";
 import { StyledButton } from "@pages/Community/CommunityPage.style";
+import { useEffect, useState } from "react";
 const CONCERNS = CONCERN_LIST;
 const notices = NOTICE_LIST;
 const qnas = QNA_LIST;
 const CommunityBoardPage = ({ type }) => {
-  const location = useLocation();
+  const [boardList, setBoardList] = useState([]);
+  useEffect(() => {
+    if (type === "Q&A") {
+      setBoardList(qnas);
+    } else {
+      setBoardList(CONCERNS);
+    }
+  }, [type]);
   return (
     <>
       <NoticeCard notice={notices[0]} />
       <NoticeCard notice={notices[1]} />
       <NoticeCard notice={notices[2]} />
       <br />
-      {type === "Q&A" ? (
-        <>
-          {qnas.map((qna) => (
-            <>
-              <QnaCard qna={qna} key={qna.id} />
-              {qna.id < qnas.length && <hr />}
-            </>
-          ))}
-        </>
-      ) : (
-        <>
-          {CONCERNS.map((concern) => (
-            <ConcernCard
-              concern={concern}
-              key={concern.id}
-            />
-          ))}
-        </>
-      )}
+      {boardList.map((board) => (
+        <CommunityCard
+          board={board}
+          key={board.id}
+          type={type}
+        />
+      ))}
       <Link
         to={PATH.COMMUNITY_REGIST(type)}
         state={{ type }}
