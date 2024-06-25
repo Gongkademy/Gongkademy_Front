11a3587122158873bfd2b61207e25bafbfd164fa
KIMHT98 <kht19684@gmail.com>
2024-06-25 22:22:58 +0900

    Refactor: CommunityPage 버튼 CommunityBoardPage로 이동

--- src/components/community/communityBoard/CommunityBoardPage.jsx
+++ src/components/community/communityBoard/CommunityBoardPage.jsx
@@ -1,41 +1,62 @@
-import { useLocation } from "react-router-dom";
+import {
+  useLocation,
+  Link,
+} from "react-router-dom";
+import { PATH } from "@router/Constants";
 import NoticeCard from "@components/community/Card/NoticeCard";
 import { NOTICE_LIST } from "@dummy/Notice";
 import { QNA_LIST } from "@dummy/Qna";
 import QnaCard from "@components/community/Card/QnaCard";
 import ConcernCard from "@components/community/Card/ConcernCard";
 import { CONCERN_LIST } from "@dummy/Concern";
+import { StyledButton } from "@pages/Community/CommunityPage.style";
 const CONCERNS = CONCERN_LIST;
 const notices = NOTICE_LIST;
 const qnas = QNA_LIST;
-const CommunityBoardPage = () => {
+const CommunityBoardPage = ({ type }) => {
   const location = useLocation();
   return (
-    <div>
-      {location.pathname.includes("Q&A") ? (
-        <div>
-          <NoticeCard notice={notices[0]} />
-          <NoticeCard notice={notices[1]} />
-          <NoticeCard notice={notices[2]} />
-          <br></br>
+    <>
+      <NoticeCard notice={notices[0]} />
+      <NoticeCard notice={notices[1]} />
+      <NoticeCard notice={notices[2]} />
+      <br />
+      {type === "Q&A" ? (
+        <>
           {qnas.map((qna) => (
             <>
               <QnaCard qna={qna} key={qna.id} />
               {qna.id < qnas.length && <hr />}
             </>
           ))}
-        </div>
+        </>
       ) : (
-        <div>
+        <>
           {CONCERNS.map((concern) => (
             <ConcernCard
               concern={concern}
               key={concern.id}
             />
           ))}
-        </div>
+        </>
       )}
-    </div>
+      <Link
+        to={PATH.COMMUNITY_REGIST(type)}
+        state={{ type }}
+        style={{
+          inline: "block",
+          width: "100%",
+          "text-align": "center",
+        }}
+      >
+        <StyledButton fill bold>
+          ✍️{" "}
+          {type === "Q&A"
+            ? "질문하기"
+            : "고민 나누기"}
+        </StyledButton>
+      </Link>
+    </>
   );
 };
 
