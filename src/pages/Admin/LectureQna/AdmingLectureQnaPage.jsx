c4a2f175bfd820ac573e8e199c6833351f622d92
mung96 <mung5079@gmail.com>
2024-07-11 20:31:37 +0900

    Feat: 관리자에 들어가는 페이지 생성

---
+++ src/pages/Admin/LectureQna/AdmingLectureQnaPage.jsx
@@ -0,0 +1,8 @@
+const AdmingLectureQnaPage = () => {
+  return (
+    <div>
+      <h1>Qna 관리</h1>
+    </div>
+  );
+};
+export default AdmingLectureQnaPage;
---
+++ src/pages/Admin/LectureRegist/AdmineLectureRegistPage.jsx
@@ -0,0 +1,8 @@
+const AdmineLectureRegistPage = () => {
+  return (
+    <div>
+      <h1>강의 등록</h1>
+    </div>
+  );
+};
+export default AdmineLectureRegistPage;
