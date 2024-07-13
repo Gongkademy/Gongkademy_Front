858448fa36e3d7643a14217b0446f780bfd664ce
mung96 <mung5079@gmail.com>
2024-07-13 15:45:58 +0900

    Feat: 관리자용 api instance구현

--- src/apis/apiConstants.js
+++ src/apis/apiConstants.js
@@ -1,5 +1,5 @@
-export const BASE_URL = import.meta.env
-  .VITE_APP_SERVICE_API_URL;
+export const BASE_URL = import.meta.env.VITE_APP_SERVICE_API_URL;
+export const ADMIN_BASE_URL = import.meta.env.VITE_APP_ADMIN_API_URL;
 
 export const END_POINT = {
   MEMBERS: "members",
@@ -8,18 +8,14 @@ export const END_POINT = {
   LOGIN: "members/login",
 
   COMMUNITY_NOTICE: "community/notice",
-  COMMUNITY_NOTICE_ARTICLE: (articleId) =>
-    `community/notice/${articleId}`,
+  COMMUNITY_NOTICE_ARTICLE: (articleId) => `community/notice/${articleId}`,
 
   COMMUNITY_CONCERN: "community/consulting",
-  COMMUNITY_CONCERN_ARTICLE: (articleId) =>
-    `community/consulting/${articleId}`,
+  COMMUNITY_CONCERN_ARTICLE: (articleId) => `community/consulting/${articleId}`,
 
   COMMUNITY_QUESTION: "community/question",
-  COMMUNITY_QUESTION_ARTICLE: (articleId) =>
-    `community/question/${articleId}`,
+  COMMUNITY_QUESTION_ARTICLE: (articleId) => `community/question/${articleId}`,
 
   COMMUNITY_REVIEW: "community/comment",
-  COMMUNITY_REVIEW_COMMENT: (commentId) =>
-    `community/comment/${commentId}`,
+  COMMUNITY_REVIEW_COMMENT: (commentId) => `community/comment/${commentId}`,
 };
