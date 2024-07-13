4a97842ee61a5738395880e014925c8db9459fbe
mung96 <mung5079@gmail.com>
2024-07-13 13:52:37 +0900

    Feat: 강좌 대표이미지,제목 등록 페이지 레이아웃 구현

--- src/pages/Admin/LectureRegist/AdminLectureRegistPage.jsx
+++ src/pages/Admin/LectureRegist/AdminLectureRegistPage.jsx
@@ -1,8 +1,18 @@
+import Button from "../../../components/common/button/Button";
+import ImageUpload from "../../../components/common/imageUpload/ImageUpload";
+import Input from "../../../components/common/input/Input";
+import PageLayout from "../../../components/common/page/PageLayout";
+import Text from "../../../components/common/text/Text";
+import { typo } from "../../../styles/style";
+
 const AdminLectureRegistPage = () => {
   return (
-    <div>
-      <h1>강의 등록</h1>
-    </div>
+    <PageLayout>
+      <Button fill>등록하기</Button>
+      <Input placeholder={"강좌 제목을 입력해주세요"} />
+      <Text typo={typo.titleRg700}>대표이미지</Text>
+      <ImageUpload width={"450px"} height={"300px"} />
+    </PageLayout>
   );
 };
 export default AdminLectureRegistPage;
