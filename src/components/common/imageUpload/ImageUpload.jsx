525bc4200dcf8eba0f2bfcd73f067d54b84902dc
mung96 <mung5079@gmail.com>
2024-07-13 13:51:38 +0900

    Feat: Image 업로드 하는 컴포넌트 구현

---
+++ src/components/common/imageUpload/ImageUpload.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import { ImageLabel, Thumnail } from "./ImageUpload.style";
+
+const ImageUpload = ({ width, height }) => {
+  const [image, setImage] = useState();
+  const [preview, setPreview] = useState();
+  const handleImageInputChange = (e) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(e.target.files[0]);
+    setImage(e.target.files[0]);
+
+    reader.onload = () => {
+      const data = reader.result;
+      setPreview(data);
+      e.target.value = "";
+    };
+  };
+
+  return (
+    <div>
+      <ImageLabel width={width} height={height}>
+        {preview ? (
+          <Thumnail src={preview} width={width} height={height} />
+        ) : (
+          <div>강좌 대표이미지 등록하기</div>
+        )}
+        <input onChange={handleImageInputChange} type="file" accept="image/*" />
+      </ImageLabel>
+    </div>
+  );
+};
+
+export default ImageUpload;
