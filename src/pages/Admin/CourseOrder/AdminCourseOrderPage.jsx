d83497a1a101e3a9f5ca86bd6b8d3bfb47332431
mung96 <mung5079@gmail.com>
2024-07-14 17:04:13 +0900

    Feat: 강의 목차 추가 API 연결

--- src/pages/Admin/CourseOrder/AdminCourseOrderPage.jsx
+++ src/pages/Admin/CourseOrder/AdminCourseOrderPage.jsx
@@ -1,8 +1,67 @@
+import { useParams } from "react-router-dom";
+import { createLecture } from "../../../apis/course/adminCourseApi";
+import Button from "../../../components/common/button/Button";
+import { Flex } from "../../../components/common/flex/Flex";
+import CurriculumItem from "../../../components/courseDetail/courseCurriculum/CurriculumItem";
+import Input from "../../../components/common/input/Input";
+import useForm from "../../../hooks/useForm";
+import { HTTP_STATUS_CODE } from "../../../apis/apiConstants";
+
 const AdminCourseOrderPage = () => {
+  const params = useParams();
+  const {
+    values,
+    messages,
+    states,
+    submitting,
+    handleChange,
+    handleSubmit,
+    response,
+  } = useForm({
+    initialValues: {
+      lectureOrder: "",
+      time: "",
+      link: "",
+      title: "",
+      courseId: params.courseId,
+    },
+    onSubmit: createLecture,
+  });
+
+  const handleFormSubmit = async (event) => {
+    await handleSubmit(event);
+    if (response.status === HTTP_STATUS_CODE.CREATED) {
+      alert("강의 등록에 성공했습니다");
+    }
+    console.log(response);
+  };
   return (
-    <div>
-      <h1>목차 관리</h1>
-    </div>
+    <>
+      <form onSubmit={handleFormSubmit}>
+        <Button fill>목차 추가하기</Button>
+        <Input
+          name={"title"}
+          placeholder={"강의 제목을 입력해주세요"}
+          onChange={handleChange}
+        />
+        <Input
+          name={"time"}
+          placeholder={"강의시간을 입력해주세요"}
+          onChange={handleChange}
+        />
+        <Input
+          name={"link"}
+          placeholder={"url을 입력해주세요"}
+          onChange={handleChange}
+        />
+        <Input
+          name={"lectureOrder"}
+          placeholder={"몇번째 강의인가요?"}
+          onChange={handleChange}
+        />
+      </form>
+      <CurriculumItem />
+    </>
   );
 };
 export default AdminCourseOrderPage;
