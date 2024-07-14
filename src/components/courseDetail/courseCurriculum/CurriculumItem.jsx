8ccf742329988f1ddbddfdf3d85653caa1c601b4
mung96 <mung5079@gmail.com>
2024-07-15 08:46:25 +0900

    Feat: 강의 목차 삭제 기능 구현

--- src/components/courseDetail/courseCurriculum/CurriculumItem.jsx
+++ src/components/courseDetail/courseCurriculum/CurriculumItem.jsx
@@ -6,12 +6,31 @@ import { COURSE_ID, LECTURE_ID } from "@pages/Service/Lecture/constants";
 import { CurriculumItemBlock } from "./CurriculumItem.style";
 import { color } from "../../../styles/style";
 import Button from "../../common/button/Button";
+import { deleteLecture } from "../../../apis/course/adminCourseApi";
+import { HTTP_STATUS_CODE } from "../../../apis/apiConstants";
 
 const CurriculumItem = ({ lecture, type }) => {
   const lectureQueryString = `?${LECTURE_ID}=${1}&${COURSE_ID}=${1}`;
   console.log(lecture);
+  const handleDeleteBtnClick = async () => {
+    const isConfirm = confirm("목차를 삭제하시겠습니까?");
+    if (isConfirm) {
+      try {
+        const response = await deleteLecture(lecture.lectureId);
+        if (response.status === HTTP_STATUS_CODE.NO_CONTENT) {
+          alert("삭제를 완료했습니다.");
+          location.reload();
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
   return (
-    <CurriculumItemBlock to={PATH.LECTURE + lectureQueryString}>
+    <CurriculumItemBlock
+      as={type === "admin" && "div"}
+      to={PATH.LECTURE + lectureQueryString}
+    >
       <Flex gap="0.75rem" align="center">
         {type === "user" && (
           <CheckIcon
@@ -28,7 +47,9 @@ const CurriculumItem = ({ lecture, type }) => {
         {type === "admin" && (
           <>
             <Button fill>수정하기</Button>
-            <Button outline>삭제하기</Button>
+            <Button outline onClick={handleDeleteBtnClick}>
+              삭제하기
+            </Button>
           </>
         )}
         <Text type="captionRg400" color={color.gray400}>
