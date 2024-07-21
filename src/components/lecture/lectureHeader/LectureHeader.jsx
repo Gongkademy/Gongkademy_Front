f14b4809abcb2c524b64964ca4f8a411cb6151c3
mung96 <mung5079@gmail.com>
2024-07-21 15:20:25 +0900

    Feat: Player페이지에서 강좌상세로 이동 기능 구현

--- src/components/lecture/lectureHeader/LectureHeader.jsx
+++ src/components/lecture/lectureHeader/LectureHeader.jsx
@@ -1,15 +1,17 @@
 import { HeaderBlock } from "./LectureHeader.style";
 
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 import { PATH } from "@router/Constants";
 import { ArrowLeftIcon } from "@assets/svg/icons";
 import { Flex } from "../../common/flex/Flex";
 import Text from "../../common/text/Text";
 import { typo } from "@styles/style";
-const LectureHeader = ({ title, courseId }) => {
+import { COURSE_ID } from "@pages/Service/Lecture/constants";
+const LectureHeader = ({ title }) => {
+  const [searchParams] = useSearchParams();
   return (
     <HeaderBlock>
-      <Link to={PATH.COURSE_DETAIL(1)}>
+      <Link to={PATH.COURSE_DETAIL(searchParams.get(COURSE_ID))}>
         <Flex align="center" height="2rem" gap="0.5rem">
           <ArrowLeftIcon width={16} height={16} />
           <Text typo={typo.bodySm700}>강의 상세</Text>
