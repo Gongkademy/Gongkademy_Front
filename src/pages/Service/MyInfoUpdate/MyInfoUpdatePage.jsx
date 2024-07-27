5a580e70d7545e1824b725a5affbd838db95b17a
mung96 <mung5079@gmail.com>
2024-07-27 14:59:50 +0900

    Design: 마이페이지 정보 수정 레이아웃 구현

--- src/pages/Service/MyInfoUpdate/MyInfoUpdatePage.jsx
+++ src/pages/Service/MyInfoUpdate/MyInfoUpdatePage.jsx
@@ -1,5 +1,26 @@
+import { FileIcon } from "@assets/svg/icons";
+import { Flex } from "@components/common/flex/Flex";
+import Input from "@components/common/input/Input";
+import { PageTitle } from "@components/common/page/PageTitle";
+import Profile from "@components/common/profile/Profile";
+import { useState } from "react";
+
 const MyInfoUpdatePage = () => {
-  return <div>회원정보 수정</div>;
+  const [newProfileImg, setNewProfileImg] = useState();
+
+  return (
+    <>
+      <PageTitle>
+        <FileIcon width={"24px"} /> 내 정보 수정
+      </PageTitle>
+
+      <Flex direction="column" gap="2rem" width="100%" maxWidth="608px">
+        <Profile width={"4rem"} height={"4rem"} onChange={setNewProfileImg} />
+        <Input label={"닉네임"} disabled />
+        <Input label={"이메일"} disabled />
+      </Flex>
+    </>
+  );
 };
 
 export default MyInfoUpdatePage;
