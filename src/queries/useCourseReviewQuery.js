5823093f98a885f9239137dfd241cfba898bd00f
mung96 <mung5079@gmail.com>
2024-08-05 08:37:02 +0900

    Feat: 수강평 API useQuery작성

---
+++ src/queries/useCourseReviewQuery.js
@@ -0,0 +1,55 @@
+import {
+  deleteCourseReview,
+  getAllCourseReviews,
+  patchCourseReview,
+  postCourseReview,
+} from "@apis/course/courseReviewApi";
+import { queryClient } from "@queries/queryClient";
+import { useMutation, useQuery } from "@tanstack/react-query";
+
+export const useCourseReviewQuery = (courseId) =>
+  useQuery({
+    queryKey: ["courseReview", courseId],
+    queryFn: () => getAllCourseReviews(courseId),
+  });
+
+export const useRegistCourseReviewMutation = () =>
+  useMutation({
+    mutationFn: (data) => postCourseReview(data),
+    onSuccess: () => {
+      queryClient.invalidateQueries(["courseReview"]);
+    },
+    onError: (error) => {
+      console.log("onError", error);
+    },
+    onSettled: () => {
+      console.log("onSettled");
+    },
+  });
+
+export const useModifyCourseReviewMutation = () =>
+  useMutation({
+    mutationFn: ({ reviewId, data }) => patchCourseReview(reviewId, data),
+    onSuccess: () => {
+      queryClient.invalidateQueries(["courseReview"]);
+    },
+    onError: (error) => {
+      console.log("onError", error);
+    },
+    onSettled: () => {
+      console.log("onSettled");
+    },
+  });
+export const useRemoveCourseReview = () =>
+  useMutation({
+    mutationFn: (reviewId) => deleteCourseReview(reviewId),
+    onSuccess: () => {
+      queryClient.invalidateQueries(["courseReview"]);
+    },
+    onError: (error) => {
+      console.log("onError", error);
+    },
+    onSettled: () => {
+      console.log("onSettled");
+    },
+  });
