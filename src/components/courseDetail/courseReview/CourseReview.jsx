13765239cb3ba24ee7c3c0ee86c45888b8f2070e
mung96 <mung5079@gmail.com>
2024-08-05 09:03:11 +0900

    Feat: 수강평 등록 기능 구현

--- src/components/courseDetail/courseReview/CourseReview.jsx
+++ src/components/courseDetail/courseReview/CourseReview.jsx
@@ -6,28 +6,46 @@ import Text from "@components/common/text/Text";
 import Select from "@components/common/select/Select";
 import Review from "../../common/review/Review";
 import { review } from "../../../dummy/Review";
+import Button from "@components/common/button/Button";
+import { useRegistCourseReviewMutation } from "@queries/useCourseReviewQuery";
+import { useParams } from "react-router-dom";
 const CourseReview = () => {
-  const [content, setContent] = useState();
+  const [content, setContent] = useState("");
   const [rating, setRating] = useState(0);
   const [order, setOrder] = useState("최신순");
-  useEffect(() => {
-    console.log(content);
-    console.log(rating);
-  }, [content]);
+  const registCourseReview = useRegistCourseReviewMutation();
+  const params = useParams();
 
   return (
     <Flex as="section" direction="column" gap="1rem">
-      <Flex align="center" gap="1rem">
-        <Rating
-          width={"1.5rem"}
-          count={5}
-          value={rating}
-          onChange={setRating}
-        />
-        <Text typo="bodyLg700">({rating})</Text>
+      <Flex justify="space-between">
+        <Flex align="center" gap="1rem">
+          <Rating
+            width={"1.5rem"}
+            count={5}
+            value={rating}
+            onChange={setRating}
+          />
+          <Text typo="bodyLg700">({rating})</Text>
+        </Flex>
+        <Button
+          fill
+          onClick={() => {
+            registCourseReview.mutate({
+              rating: rating,
+              content: content,
+              courseId: params.courseId,
+            });
+          }}
+        >
+          등록하기
+        </Button>
       </Flex>
 
-      <TextArea placeholder={"수강평을 작성해주세요"} onChange={setContent} />
+      <TextArea
+        placeholder={"수강평을 작성해주세요"}
+        onChange={(e) => setContent(e.target.value)}
+      />
 
       <Select
         options={["최신순", "좋아요순", "높은 평점순", "낮은 평점순"]}
